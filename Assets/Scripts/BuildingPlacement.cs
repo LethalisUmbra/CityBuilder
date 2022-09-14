81927fe6fa57cbb452c4e28091e273b7e06869d4
LethalisUmbra <anibal.cabrera2000@gmail.com>
2022-09-14 15:05:15 -0300

    Initial commit

---
+++ Assets/Scripts/BuildingPlacement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildingPlacement : MonoBehaviour
+{
+    private bool currentlyPlacing;
+    private bool currentlyBulldozering;
+
+    private BuildingPreset curBuildingPreset;
+
+    private float indicatorUpdateRate = 0.05f;
+    private float lastUpdateTime;
+    private Vector3 curIndicatorPos;
+
+    public GameObject placementIndicator;
+    public GameObject bulldozeIndicator;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            CancelBuildingPlacement();
+
+        if (Time.time - lastUpdateTime > indicatorUpdateRate)
+        {
+            lastUpdateTime = Time.time;
+            curIndicatorPos = Selector.instance.GetCurTilePosition();
+
+            if (currentlyPlacing)
+                placementIndicator.transform.position = curIndicatorPos;
+            else if (currentlyBulldozering)
+                bulldozeIndicator.transform.position = curIndicatorPos;
+        }
+
+        if (Input.GetMouseButtonDown(0) && currentlyPlacing)
+            PlaceBuilding();
+        else if (Input.GetMouseButtonDown(0) && currentlyBulldozering)
+            Bulldoze();
+    }
+
+    public void BeginNewBuildingPlacement (BuildingPreset preset)
+    {
+        // Check Money
+        currentlyPlacing = true;
+        curBuildingPreset = preset;
+        placementIndicator.SetActive(true);
+        placementIndicator.transform.position = new Vector3(0, -99, 0);
+    }
+
+    void CancelBuildingPlacement ()
+    {
+        currentlyPlacing = false;
+        currentlyBulldozering = false;
+        bulldozeIndicator.SetActive(false);
+        placementIndicator.SetActive(false);
+    }
+
+    public void ToggleBulldoze()
+    {
+        currentlyBulldozering = !currentlyBulldozering;
+        bulldozeIndicator.SetActive(currentlyBulldozering);
+        placementIndicator.transform.position = new Vector3(0, -99, 0);
+    }
+
+    void PlaceBuilding()
+    {
+        GameObject buildingObj = Instantiate(curBuildingPreset.prefab, curIndicatorPos, Quaternion.identity);
+        // Tell city script
+        CancelBuildingPlacement();
+    }
+
+    void Bulldoze()
+    {
+
+    }
+}
